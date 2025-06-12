c55ba0aafc0ba3e217379f941a9a6cea4bfb4591
Paulo Henrique Morais dos Santos <73863314+PMorais13@users.noreply.github.com>
2025-06-11 22:36:03 -0300

    Increase troll size

--- constants.js
+++ constants.js
@@ -26,7 +26,7 @@ const GAME_CONSTANTS = {
     miniom: { hp: 2, speed: 1.8, size: 60 },
     tanker: { hp: 8, speed: 1, size: 100 },
     voador: { hp: 1, speed: 2.5, size: 50 },
-    troll: { hp: 30, speed: 0.7, size: 120 },
+    troll: { hp: 30, speed: 0.7, size: 240 },
   },
   ENEMY_SPAWN_WEIGHTS: {
     miniom: 60,
